f090c4e58a493d51aa60ad06f07ec5a0f4aa96c9
enhasya <enhasya@outlook.co.id>
2024-03-19 01:41:35 +0700

    update

--- app/student/components/table.jsx
+++ app/student/components/table.jsx
@@ -11,7 +11,7 @@ const ComponentTable = () => {
           <th className="px-4 pt-4 pb-4 text-sm font-semibold text-start">
             Mata Pelajaran
           </th>
-          <th className="px-4 pt-4 pb-4 text-sm font-semibold text-start">
+          <th className="pr-4 pt-4 pb-4 text-sm font-semibold text-start">
             Nilai (Angka)
           </th>
           <th className="pr-4 pt-4 pb-4 text-sm font-semibold text-start">
@@ -25,15 +25,15 @@ const ComponentTable = () => {
           <td className="px-4 pt-0 pb-4 text-xs font-medium text-start">
             Sains
           </td>
-          <td className="px-4 pt-0 pb-4 text-xs font-medium text-start">100</td>
+          <td className="pr-4 pt-0 pb-4 text-xs font-medium text-start">100</td>
           <td className="pr-4 pt-0 pb-4 text-xs font-medium text-start">A</td>
         </tr>
         <tr>
-          <td className="pl-4 pt-0 pb-4 text-xs font-medium text-start">1</td>
+          <td className="pl-4 pt-0 pb-4 text-xs font-medium text-start">2</td>
           <td className="px-4 pt-0 pb-4 text-xs font-medium text-start">
             Matematika
           </td>
-          <td className="px-4 pt-0 pb-4 text-xs font-medium text-start">85</td>
+          <td className="pr-4 pt-0 pb-4 text-xs font-medium text-start">85</td>
           <td className="pr-4 pt-0 pb-4 text-xs font-medium text-start">AB</td>
         </tr>
       </tbody>

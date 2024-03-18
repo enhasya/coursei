8fea87f4dddeee744783577ec5d41d34da5da3ea
enhasya <enhasya@outlook.co.id>
2024-03-19 01:33:57 +0700

    update

--- components/navigations/student.jsx
+++ components/navigations/student.jsx
@@ -19,7 +19,7 @@ const NavigationStudent = () => {
 
   return (
     <nav className="bg-[#FFFFFF] flex flex-row lg:flex-col gap-2 items-center lg:items-start justify-between w-full h-auto lg:h-full p-4 lg:p-8">
-      <button className="bg-[#F4F4F4] transition-all hover:bg-[#F4F4F4]/80 flex lg:hidden flex-row gap-4 items-center justify-start w-auto lg:w-full h-auto rounded-lg p-2 lg:p-4 mt-0 lg:mt-4">
+      <button className="bg-[#F4F4F4] transition-all hover:bg-[#F4F4F4]/80 flex lg:hidden flex-row gap-4 items-center justify-start w-auto lg:w-full h-auto rounded-lg p-4 mt-0 lg:mt-4">
         <Image src={icon.user} width="16" height="16" />
         <p className="hidden lg:flex text-[#292D32] font-medium text-sm mt-0">
           Masuk sebagai C.73.2918.2
@@ -108,7 +108,7 @@ const NavigationStudent = () => {
             </React.Fragment>
           )}
         </div>
-        <button className="bg-[#292D32] transition-all hover:bg-[#292D32]/80 flex flex-row gap-4 items-center justify-center w-auto lg:w-full h-auto rounded-lg p-2 lg:p-4 mt-0 lg:mt-4">
+        <button className="bg-[#292D32] transition-all hover:bg-[#292D32]/80 flex flex-row gap-4 items-center justify-center w-auto lg:w-full h-auto rounded-lg p-4 mt-0 lg:mt-4">
           <p className="hidden lg:flex text-[#F4F4F4] font-medium text-sm mt-0">
             Keluar
           </p>

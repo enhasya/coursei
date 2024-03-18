bce5ce50718644a58b608ace70fb4ec4f76da0e6
enhasya <enhasya@outlook.co.id>
2024-03-19 01:11:11 +0700

    update

--- components/layouts/auth.jsx
+++ components/layouts/auth.jsx
@@ -1,7 +1,12 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
+
+const activeSession = {
+  auth: "auth",
+  recovery: "recovery",
+};
 
 const image = {
   cover: require("@/shared/assets/brandon-vazquez-koC5eG6v6ew-unsplash.jpg"),
@@ -11,9 +16,12 @@ const icon = {
   user: require("@/shared/icons/user-black.svg"),
   password: require("@/shared/icons/password-black.svg"),
   signIn: require("@/shared/icons/sign-in-white.svg"),
+  arrowLeft: require("@/shared/icons/arrow-left-white.svg"),
 };
 
 const LayoutAuth = () => {
+  const [isActive, setIsActive] = useState(activeSession.auth);
+
   return (
     <main className="flex flex-col lg:flex-row gap-0 items-center justify-center w-full h-screen p-8">
       <div className="bg-white hidden lg:flex flex-col w-full lg:w-[440px] h-auto lg:h-[560px] rounded-none lg:rounded-l-lg p-0">
@@ -31,33 +39,60 @@ const LayoutAuth = () => {
             Otentikasi diperlukan untuk mengakses dashboard
           </h2>
         </div>
-        <form className="flex flex-col gap-0 mt-8 lg:mt-0 w-full">
-          <p className="text-[#292D32] font-medium text-xs mt-4">Username</p>
-          <div className="bg-[#292D32]/5 ring-1 ring-[#292D32]/10 transition-all hover:ring-[#292D32]/15 flex flex-row gap-4 items-center justify-start w-full h-auto rounded-lg p-4 mt-2">
-            <Image src={icon.user} width="14" height="14" />
-            <input
-              className="bg-transparent outline-none w-full text-[#292D32] font-medium text-xs"
-              type="text"
-              placeholder="C.73.2918.2"
-            />
-          </div>
-          <p className="text-[#292D32] font-medium text-xs mt-4">Password</p>
-          <div className="bg-[#292D32]/5 ring-1 ring-[#292D32]/10 transition-all hover:ring-[#292D32]/15 flex flex-row gap-4 items-center justify-start w-full h-auto rounded-lg p-4 mt-2">
-            <Image src={icon.password} width="14" height="14" />
-            <input
-              className="bg-transparent outline-none w-full text-[#292D32] font-medium text-xs"
-              type="password"
-              placeholder="password123"
-            />
-          </div>
-          <p className="text-[#292D32] font-medium text-xs text-end hover:underline mt-4">
-            Lupa password?
-          </p>
-          <button className="bg-[#292D32] transition-all hover:bg-[#292D32]/80 flex flex-row gap-4 items-center justify-center w-full h-auto rounded-lg p-4 mt-4">
-            <p className="text-[#F4F4F4] font-medium text-sm mt-0">Masuk</p>
-            <Image src={icon.signIn} width="16" height="16" />
-          </button>
-        </form>
+        {isActive === activeSession.auth && (
+          <form className="flex flex-col gap-0 mt-8 lg:mt-0 w-full items-end justify-center">
+            <p className="text-[#292D32] font-medium text-xs text-start w-full mt-4">
+              Username
+            </p>
+            <div className="bg-[#292D32]/5 ring-1 ring-[#292D32]/10 transition-all hover:ring-[#292D32]/15 flex flex-row gap-4 items-center justify-start w-full h-auto rounded-lg p-4 mt-2">
+              <Image src={icon.user} width="14" height="14" />
+              <input
+                className="bg-transparent outline-none w-full text-[#292D32] font-medium text-xs"
+                type="text"
+                placeholder="C.73.2918.2"
+              />
+            </div>
+            <p className="text-[#292D32] font-medium text-xs text-start w-full mt-4">
+              Password
+            </p>
+            <div className="bg-[#292D32]/5 ring-1 ring-[#292D32]/10 transition-all hover:ring-[#292D32]/15 flex flex-row gap-4 items-center justify-start w-full h-auto rounded-lg p-4 mt-2">
+              <Image src={icon.password} width="14" height="14" />
+              <input
+                className="bg-transparent outline-none w-full text-[#292D32] font-medium text-xs"
+                type="password"
+                placeholder="password123"
+              />
+            </div>
+            <button
+              className="mt-4"
+              onClick={() => setIsActive(activeSession.recovery)}
+            >
+              <p className="text-[#292D32] font-medium text-xs text-end hover:underline mt-0">
+                Lupa password?
+              </p>
+            </button>
+            <button className="bg-[#292D32] transition-all hover:bg-[#292D32]/80 flex flex-row gap-4 items-center justify-center w-full h-auto rounded-lg p-4 mt-4">
+              <p className="text-[#F4F4F4] font-medium text-sm mt-0">Masuk</p>
+              <Image src={icon.signIn} width="16" height="16" />
+            </button>
+          </form>
+        )}
+        {isActive === activeSession.recovery && (
+          <React.Fragment>
+            <h2 className="text-[#292D32]/80 font-medium text-sm mt-8 lg:mt-0">
+              Hubungi administrator untuk mengurus akun anda
+            </h2>
+            <button
+              onClick={() => {
+                setIsActive(activeSession.auth);
+              }}
+              className="bg-[#292D32] transition-all hover:bg-[#292D32]/80 flex flex-row gap-4 items-center justify-center w-full h-auto rounded-lg p-4 mt-4"
+            >
+              <Image src={icon.arrowLeft} width="16" height="16" />
+              <p className="text-[#F4F4F4] font-medium text-sm mt-0">Kembali</p>
+            </button>
+          </React.Fragment>
+        )}
       </div>
     </main>
   );

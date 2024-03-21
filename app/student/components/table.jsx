072c63e3a1cdc2693eac166d168dce9f32ddd254
enhasya <enhasya@outlook.co.id>
2024-03-22 05:24:39 +0700

    update

--- app/student/components/table.jsx
+++ app/student/components/table.jsx
@@ -2,42 +2,50 @@ import React from "react";
 
 const ComponentTable = () => {
   return (
-    <table className="bg-[#FFFFFF] w-full rounded-lg">
-      <thead>
-        <tr>
-          <th className="pl-4 pt-4 pb-4 text-sm font-semibold text-start">
-            No
-          </th>
-          <th className="px-4 pt-4 pb-4 text-sm font-semibold text-start">
-            Mata Pelajaran
-          </th>
-          <th className="pr-4 pt-4 pb-4 text-sm font-semibold text-start">
-            Nilai (Angka)
-          </th>
-          <th className="pr-4 pt-4 pb-4 text-sm font-semibold text-start">
-            Nilai (Huruf)
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td className="pl-4 pt-0 pb-4 text-xs font-medium text-start">1</td>
-          <td className="px-4 pt-0 pb-4 text-xs font-medium text-start">
-            Sains
-          </td>
-          <td className="pr-4 pt-0 pb-4 text-xs font-medium text-start">100</td>
-          <td className="pr-4 pt-0 pb-4 text-xs font-medium text-start">A</td>
-        </tr>
-        <tr>
-          <td className="pl-4 pt-0 pb-4 text-xs font-medium text-start">2</td>
-          <td className="px-4 pt-0 pb-4 text-xs font-medium text-start">
-            Matematika
-          </td>
-          <td className="pr-4 pt-0 pb-4 text-xs font-medium text-start">85</td>
-          <td className="pr-4 pt-0 pb-4 text-xs font-medium text-start">AB</td>
-        </tr>
-      </tbody>
-    </table>
+    <div className="bg-[#FFFFFF] ring-1 ring-[#292D32]/5 transition-all hover:ring-[#292D32]/10 overflow-x-auto w-full rounded-lg">
+      <table className="w-full rounded-lg">
+        <thead>
+          <tr>
+            <th className="pl-4 pt-4 pb-4 text-sm font-semibold text-start">
+              No
+            </th>
+            <th className="px-4 pt-4 pb-4 text-sm font-semibold text-start">
+              Mata Pelajaran
+            </th>
+            <th className="pr-4 pt-4 pb-4 text-sm font-semibold text-start">
+              Nilai (Angka)
+            </th>
+            <th className="pr-4 pt-4 pb-4 text-sm font-semibold text-start">
+              Nilai (Huruf)
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="pl-4 pt-4 pb-4 text-xs font-medium text-start">1</td>
+            <td className="px-4 pt-4 pb-4 text-xs font-medium text-start truncate">
+              Sains
+            </td>
+            <td className="pr-4 pt-4 pb-4 text-xs font-medium text-start">
+              100
+            </td>
+            <td className="pr-4 pt-4 pb-4 text-xs font-medium text-start">A</td>
+          </tr>
+          <tr>
+            <td className="pl-4 pt-4 pb-4 text-xs font-medium text-start">2</td>
+            <td className="px-4 pt-4 pb-4 text-xs font-medium text-start truncate">
+              Matematika
+            </td>
+            <td className="pr-4 pt-4 pb-4 text-xs font-medium text-start">
+              85
+            </td>
+            <td className="pr-4 pt-4 pb-4 text-xs font-medium text-start">
+              AB
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
